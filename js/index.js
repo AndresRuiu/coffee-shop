a414fcfc5ec9270550be114e78e3c5176a4cbaa2
AndresRuiu <andresruiu@gmail.com>
2023-11-05 04:47:39 -0300

    modificaciones

--- js/index.js
+++ js/index.js
@@ -18,3 +18,69 @@ const ListItem = document.querySelectorAll("#landing-header li")
             MenuBD.style.visibility= "hidden"
         })
     })
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var openButton = document.querySelector('.open-button');
+        var closeButton = document.querySelector('.close-button');
+        var header = document.querySelector('#landing-header');
+    
+        if(openButton && closeButton && header) {
+            openButton.addEventListener('click', function() {
+                header.style.display = 'block';
+            });
+    
+            closeButton.addEventListener('click', function() {
+                header.style.display = 'none';
+            });
+        }
+    });
+    
+
+
+    async function mostrarCafes() {
+      const response = await fetch('../data/productos.json');
+      const data = await response.json();
+    
+      // Accede a la clave "cafes" en el objeto JSON
+      const cafes = data.cafes;
+    
+      const cafesDestacados = cafes.filter(cafe => cafe.destacado === true);
+    
+      const template = document.querySelector("#cafe-card-template");
+      const container = document.querySelector("#cafes-destacados");
+    
+      const fragment = document.createDocumentFragment();
+    
+      cafesDestacados.forEach(cafe => {
+        const instance = template.content.cloneNode(true);
+    
+        instance.querySelector("#img-cafe").src = cafe.imagen;
+        instance.querySelector("#descripcion-cafe").textContent = cafe.descripcion;
+        instance.querySelector("#nombre-cafe").textContent = cafe.nombre;
+        instance.querySelector("#precio-cafe").textContent = `$${cafe.precio}`;
+    
+        fragment.appendChild(instance);
+      });
+    
+      container.appendChild(fragment);
+    }
+    
+    mostrarCafes();
+
+const cafesDestacados = document.getElementById("cafes-destacados");
+const scrollLeft = document.getElementById("scroll-left");
+const scrollRight = document.getElementById("scroll-right");
+  
+scrollLeft.addEventListener("click", () => {
+  cafesDestacados.scroll({
+    left: cafesDestacados.scrollLeft - cafesDestacados.offsetWidth,
+    behavior: "smooth",
+  });
+});
+
+scrollRight.addEventListener("click", () => {
+  cafesDestacados.scroll({
+    left: cafesDestacados.scrollLeft + cafesDestacados.offsetWidth,
+    behavior: "smooth",
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./**/*.html", "./js/**/*.js"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
